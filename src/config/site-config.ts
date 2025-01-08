02a10581b11152a4f90f64c34594d86198c4aa3c
José Quiñónez <jquinonez@tsc.com.gt>
2025-01-08 09:13:34 -0600

    Agregar diseño de blog y componentes para publicaciones

---
+++ src/config/site-config.ts
@@ -0,0 +1,14 @@
+
+
+export const siteConfig = {
+  title: "José Quinonez' Blog",
+  description: "My personal blog about web development, design, and life.",
+  url: "https://josequinonez.dev",
+  author: "José Quinonez",
+  twitter: "@josequinonez",
+  github: "https://github.com/josequinonez",
+  linkedin: "https://www.linkedin.com/in/josequinonez",
+  stackoverflow: "https://stackoverflow.com/users/11203112/jose-quinonez",
+  youtube: "https://www.youtube.com/channel/UC9-rYyUMsnEBK8G8fCyrXXA",
+  email: "josequinonez@gmail.com",
+};
